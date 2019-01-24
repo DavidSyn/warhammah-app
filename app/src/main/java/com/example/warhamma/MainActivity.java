9689acd43f2e52a87e2c24f88eff3490b39b8e65
Andras <proci85@hotmail.com>
2019-01-24 01:14:26 +0100

    Displaying web pages ok
    The functionality is added to display a single webview (e.g. a web page), either using a page on the internet or by manually feeding an unencoded HTML into a function.
    
    Practically we can focus on the webpage and using the app as a mean.
    
    /Andras

--- app/src/main/java/com/example/warhamma/MainActivity.java
+++ app/src/main/java/com/example/warhamma/MainActivity.java
@@ -2,13 +2,44 @@ package com.example.warhamma;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Base64;
+import android.webkit.WebView;
 
+import static android.util.Base64.*;
 
 public class MainActivity extends AppCompatActivity {
+
+    WebView myWebView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        WebView myWebView = (WebView) findViewById(R.id.webview);
+
+        // NOTE: either load a webpage or an unencoded HTML, as there is only webview, loading both crashes the app!
+
+        // code base for web page input
+        //loadURL("https://www.google.se/");
+
+        //code base for static html input
+        String unencodedHtml =
+                "<html><h1>hello David!</h1><table><tr><th>Type</th><th>Quantity</th></tr></tr><th>Orc</th><th>10</th></tr><tr><th>Marine</th><th>20</th></tr></table></html>";
+        //loadUnencodedHTMLAsString(unencodedHtml);
+        String encodedHtml = encodeToString(unencodedHtml.getBytes(), NO_PADDING);
+        myWebView.loadData(encodedHtml, "text/html", "base64");
+
+        // TODO this is where the web page should be loaded
+        //myWebView.loadUrl("https://www.google.se/");
+
+        // src:
+        //https://developer.android.com/guide/webapps/webview#java
+    }
+
+    // loads an url into myWebView
+    public void loadURL(String inputURL){
+        myWebView.loadUrl(inputURL);
     }
 }
