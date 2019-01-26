a0745b1c8f0a6a4f90097428734a7bb4cd97bc67
Andras <proci85@hotmail.com>
2019-01-26 22:22:27 +0100

    connection to warhamma-app working
    connection to warhamma app working. javascript ok

--- app/src/main/java/com/example/warhamma/MainActivity.java
+++ app/src/main/java/com/example/warhamma/MainActivity.java
@@ -18,18 +18,23 @@ public class MainActivity extends AppCompatActivity {
 
 
         WebView myWebView = (WebView) findViewById(R.id.webview);
+        myWebView.getSettings().setJavaScriptEnabled(true);
+        myWebView.getSettings().setJavaScriptCanOpenWindowsAutomatically(true);
 
         // NOTE: either load a webpage or an unencoded HTML, as there is only webview, loading both crashes the app!
 
         // code base for web page input
-        //loadURL("https://www.google.se/");
+        // loadURL("https://www.google.se/");
+        //myWebView.loadUrl("https://www.google.se/");
+        myWebView.loadUrl("http://warhammahweb.azurewebsites.net/");
+
 
         //code base for static html input
-        String unencodedHtml =
-                "<html><h1>hello David!</h1><table><tr><th>Type</th><th>Quantity</th></tr></tr><th>Orc</th><th>10</th></tr><tr><th>Marine</th><th>20</th></tr></table></html>";
+        //String unencodedHtml =
+           //     "<html><h1>hello David!</h1><table><tr><th>Type</th><th>Quantity</th></tr></tr><th>Orc</th><th>10</th></tr><tr><th>Marine</th><th>20</th></tr></table></html>";
         //loadUnencodedHTMLAsString(unencodedHtml);
-        String encodedHtml = encodeToString(unencodedHtml.getBytes(), NO_PADDING);
-        myWebView.loadData(encodedHtml, "text/html", "base64");
+        //String encodedHtml = encodeToString(unencodedHtml.getBytes(), NO_PADDING);
+        //myWebView.loadData(encodedHtml, "text/html", "base64");
 
         // src:
         //https://developer.android.com/guide/webapps/webview#java
